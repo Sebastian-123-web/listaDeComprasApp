5564050b3a2e7806064a871afc77644f5bf4074f
Sebastian-123-web <sebastian09_26@hotmail.com>
2026-02-10 15:11:33 -0500

    Se esta corrigiendo el error del icon y manifest.json

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,20 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-
-import Carrito from "../public/carrito.gif"
+import { useEffect } from "react"
+import { useNavigate } from "react-router"
 
 function Home() {
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate("/mylists");
-    }, 3000);
+      navigate("/mylists")
+    }, 3000)
 
-    return () => clearTimeout(timer);
-  }, [navigate]);
+    return () => clearTimeout(timer)
+  }, [navigate])
 
   return (
     <div className="flex h-screen items-center justify-center bg-white">
-      <img src={Carrito} alt="" />
+      <img src="/carrito.gif" alt="" />
     </div>
   );
 }
